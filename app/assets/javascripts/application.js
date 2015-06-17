a25e3c1bf62f9240424c2048759b03fc6e7f84b0
Justin Herrick <justin@theironyard.com>
2015-06-17 17:10:09 -0500

    Alterations.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,21 +1,9 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
+/* global app */
 //= require jquery
-//= require jquery_ujs
 //= require underscore
 //= require backbone
 //= require react
+//= require moment
 //= require_directory ./components
 //= require_tree .
 
-app.navigate('edit');
--- app/assets/javascripts/components/edit.js.jsx
+++ app/assets/javascripts/components/edit.js.jsx
@@ -1,9 +1,13 @@
+/* global React */
 var Edit = React.createClass({
+  clickHandler: function (event) {
+      window.href = '/#profile'
+  },
   render: function () {
     return (
       <div>
         <h1>Edit Profile</h1>
-        <a href='/'>Cancel</a>
+        <button onClick={this.clickHandler}>Cancel</button>
       </div>
     );
   }
