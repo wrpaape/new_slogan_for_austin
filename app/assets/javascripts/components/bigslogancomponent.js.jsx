f4b517d02799f914ec1843c3007cc3e31b64def7
Kristen Rieger <karieger1@gmail.com>
2015-06-21 15:41:12 -0500

    some styling, nav component

--- app/assets/javascripts/components/bigslogancomponent.js.jsx
+++ app/assets/javascripts/components/bigslogancomponent.js.jsx
@@ -3,7 +3,7 @@ var BigSlogan = React.createClass({
     render: function () {
         return (
           <div>
-            <h1> Big Slogan </h1>
+            <h1><div className="bigslogan"> Big Slogan </div></h1>
            
           </div>
         );
--- app/assets/javascripts/components/inputnewslogancomponent.js.jsx
+++ app/assets/javascripts/components/inputnewslogancomponent.js.jsx
@@ -8,16 +8,30 @@ var InputSlogan = React.createClass ({
 				<button type= "submit">Submit </button>
 			</form>
 		);
-	},
+	
 //new slogan function
 	submitSlogan: function(e) {
 		e.preventDefault();
 		var newsloganinput = new SloganModel({
 			body: $("#newslogantext").val();
 			
-		});
+		})
 
 	});
+	
+	ifNotLoggedIn: function(e) {
+		e.preventDefault();
+		$("#newslogantext").onClick
+		//when newslogantext is clicked and user is not logged in,
+		//trigger an alert (below as swal) that directs user to log in page
+		//aaaand WTF?
+	}
+	swal({   title: "HTML <small>Title</small>!",
+	   text: "A custom <span style="
+	   color:#F8BB86">html<span> message.",   
+	   html: true 
+	});
 		
+	
 });
 
