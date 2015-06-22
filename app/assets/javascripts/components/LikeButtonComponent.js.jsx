59fc30f8ad50b403752f21022ec496f2bf16dadf
Keith Reynolds <keithreynoldsworld@gmail.com>
2015-06-21 20:09:40 -0500

    merge and register

--- app/assets/javascripts/components/LikeButtonComponent.js.jsx
+++ app/assets/javascripts/components/LikeButtonComponent.js.jsx
@@ -11,17 +11,42 @@ var LikeButton = React.createClass({
     },
         
     addLike: function(){
-       
-var numLikes = this.props.slogan.get('likes')+1;
-this.props.slogan.set({likes:numLikes});
-var ppp = new RateModel({
-    hates: 0,
-    likes: 1,
-    slogan_id: this.props.slogan.get('id')
-});
+       console.log('addLike')
+       var self = this;
 
-ppp.save();
+        // var rrr = new RateModel({
+        //     hates: 1,
+        //     likes: 0,
+        //     slogan_id: this.props.slogan.get('id')
+        // });
 
+        // rrr.save({
+        //     success: function(result) {
+        //       console.log(result);
+        //       console.log(result.hates);
+        //       var numHates = result.hates;
+        //       this.props.slogan.set({hates:numHates});
+
+        //     }
+        // });
+        $.post(
+            '/rates',
+            {
+                hates: 0,
+                likes: 1,
+                slogan_id: this.props.slogan.get('id')
+            },
+            'json'
+          )
+          .success(function(result) {
+            self.props.slogan.set({likes:result.likes, hates:result.hates});
+              console.log(result);
+          })
+          .error(function(error) {
+            // console.log(error);
+            // $('.porkbelly').append('<h1>your password or name is incorrect or you need to register</h1>');
+          })
+          
     // correctSlogan.set({hates: correctSlogan.get('hates')+1});
 
     }
